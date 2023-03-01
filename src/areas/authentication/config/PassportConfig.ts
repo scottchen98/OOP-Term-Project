871f4db29b344649bf6647d88d9728e16d0f0e33
Scott Chen <scott_chen@Scotts-MacBook-Pro.local>
2023-02-28 21:38:14 -0800

    set up passport configuration and authentication service mock

--- src/areas/authentication/config/PassportConfig.ts
+++ src/areas/authentication/config/PassportConfig.ts
@@ -3,4 +3,37 @@
 //----------------------------------------
 // 🚀 Configure Passport.js Local Authentication in this file
 //    Ensure code is fully typed wherever possible (unless inference can be made)
-export default class PassportConfig {}
+import passport from "passport";
+import { PassportStrategy } from "../../../interfaces/strategy.interface";
+import { MockAuthenticationService } from "../services";
+
+export default class PassportConfig {
+  constructor(strategies: PassportStrategy[]) {
+    this.addStrategies(strategies);
+    this.serializeUser();
+    this.deserializeUser();
+  }
+  private addStrategies(strategies: PassportStrategy[]): void {
+    strategies.forEach((passportStrategy: PassportStrategy) => {
+      passport.use(passportStrategy.name, passportStrategy.strategy);
+    });
+  }
+
+  private serializeUser() {
+    passport.serializeUser(function (user: Express.User, done: (err: any, id: number) => void) {
+      done(null, user.id);
+    });
+  }
+
+  private deserializeUser() {
+    passport.deserializeUser(async function (id: number, done: (err: any, user?: Express.User | false | null) => void) {
+      const mockAuthentication = new MockAuthenticationService();
+      let user = await mockAuthentication.getUserById(id);
+      if (user) {
+        done(null, user);
+      } else {
+        done({ message: "User not found" }, null);
+      }
+    });
+  }
+}
