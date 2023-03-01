9803c58177d6fa6e758bfee81faeeeb32d2853aa
Scott Chen <scott_chen@Scotts-MacBook-Pro.local>
2023-03-01 15:45:00 -0800

    add SearchController to App

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,13 @@ import LandingController from "./areas/landing/controllers/Landing.controller";
 import AuthenticationController from "./areas/authentication/controllers/Authentication.controller";
 import { MockAuthenticationService } from "./areas/authentication/services/Authentication.service.mock";
 import { PostService, MockPostService } from "./areas/post/services";
+import SearchController from "./areas/search/controllers/search.controller";
 
 const server = new App([
   new LandingController(),
   new PostController(new MockPostService()),
   new AuthenticationController(new MockAuthenticationService()),
+  new SearchController(),
 ]);
 
 server.start();
