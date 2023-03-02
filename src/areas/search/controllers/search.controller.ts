0d2fcbcdc6ed1d74cbd0b0ad0a7f85ea70945173
Scott Chen <scott_chen@Scotts-MacBook-Pro.local>
2023-03-01 23:48:14 -0800

    implement search route

--- src/areas/search/controllers/search.controller.ts
+++ src/areas/search/controllers/search.controller.ts
@@ -1,20 +1,30 @@
 import express from "express";
 import IController from "../../../interfaces/controller.interface";
+import ISearchService from "../services/ISearchService";
+import { MockSearchService } from "../services/Search.service.mock";
+import { SearchPostViewModel } from "../views/searchPostViewModel";
+import { SearchUserViewModel } from "../views/searchUserViewModel";
 
 class SearchController implements IController {
   public path = "/search";
   public router = express.Router();
 
-  constructor() {
+  constructor(searchService: ISearchService) {
     this.initializeRoutes();
   }
 
   private initializeRoutes() {
-    this.router.post(`${this.path}`, this.searchQuery);
+    this.router.get(`${this.path}`, this.searchQuery);
   }
 
-  private searchQuery = (req: express.Request, res: express.Response) => {
-    res.render("search/views/search.ejs");
+  private searchQuery = async (req: express.Request, res: express.Response) => {
+    const query = String(req.query.query);
+    const mockSearch = new MockSearchService();
+    const foundUsers = await mockSearch.searchUsers(query);
+    const foundPosts = await mockSearch.searchPosts(query);
+    const fullNames = foundUsers.map((user) => new SearchUserViewModel(user));
+    const posts = foundPosts.map((post) => new SearchPostViewModel(post));
+    res.render("search/views/search.ejs", { fullNames, posts });
   };
 }
 
