fa4210e217e5d2d8f7373ebd33f8fe1c5934f6d3
Scott Chen <scott_chen@Scotts-MacBook-Pro.local>
2023-03-02 00:19:30 -0800

    pass a MockSearchService instance as an argument

--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,13 @@ import AuthenticationController from "./areas/authentication/controllers/Authent
 import { MockAuthenticationService } from "./areas/authentication/services/Authentication.service.mock";
 import { PostService, MockPostService } from "./areas/post/services";
 import SearchController from "./areas/search/controllers/search.controller";
+import { MockSearchService } from "./areas/search/services/Search.service.mock";
 
 const server = new App([
   new LandingController(),
   new PostController(new MockPostService()),
   new AuthenticationController(new MockAuthenticationService()),
-  new SearchController(),
+  new SearchController(new MockSearchService()),
 ]);
 
 server.start();
