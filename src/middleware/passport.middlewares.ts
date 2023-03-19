d31c97094308f3fdc5506c32220ff499b01ec847
Scott Chen <scott_chen@Scotts-MacBook-Pro.local>
2023-03-18 17:17:47 -0700

    add datatype to app variable

--- src/middleware/passport.middlewares.ts
+++ src/middleware/passport.middlewares.ts
@@ -2,8 +2,9 @@ import { MockAuthenticationService } from "../areas/authentication/services";
 import passport from "passport";
 import PassportConfig from "../areas/authentication/config/PassportConfig";
 import localStrategy from "./passportStrategies/localStrategy";
+import express from "express";
 
-module.exports = (app) => {
+module.exports = (app: express.Application) => {
   app.use(passport.initialize());
   app.use(passport.session());
   // Use PassportConfig class here
