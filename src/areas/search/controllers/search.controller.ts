c2822ab6c045f838987dd27594f23ae829cef16c
Scott Chen <scott_chen@Scotts-MacBook-Pro.local>
2023-03-19 14:38:41 -0700

    update to currently logged-in user data and format incoming User/Post data

--- src/areas/search/controllers/search.controller.ts
+++ src/areas/search/controllers/search.controller.ts
@@ -5,6 +5,10 @@ import IPostService from "../../post/services/IPostService";
 import { database as db } from "../../../model/fakeDB";
 import { MockPostService } from "../../post/services/Post.service.mock";
 import { ensureAuthenticated } from "../../../middleware/authentication.middleware";
+import { SearchPostViewModel } from "../views/searchPostViewModel";
+import { SearchUserViewModel } from "../views/searchUserViewModel";
+import IUser from "../../../interfaces/user.interface";
+import IPost from "../../../interfaces/post.interface";
 
 class SearchController implements IController {
   public path = "/search";
@@ -20,17 +24,20 @@ class SearchController implements IController {
   }
 
   private search = async (req: Request, res: Response) => {
-    const currentUser = "billgates";
-    // req.user?.username
+    const currentUser = req.user.username;
     const mock = new MockPostService();
     const searchFor = String(req.query.query);
     const resultUsers = mock.searchUser(searchFor);
     const resultPosts = mock.searchPost(searchFor);
     const currentFollowing = db.users.filter((user) => user.username === currentUser)[0].following;
 
+    // format resultUsers and resultPosts
+    const formattedUsers = resultUsers.map((user: IUser) => new SearchUserViewModel(user).resultUser);
+    const formattedPosts = resultPosts.map((post: IPost) => new SearchPostViewModel(post).resultPost);
+
     res.render("search/views/search", {
-      resultUsers: resultUsers,
-      resultPosts: resultPosts,
+      resultUsers: formattedUsers,
+      resultPosts: formattedPosts,
       getUsernameById: mock.getUsernameById,
       checkFollowing: mock.checkFollowing,
       currentFollowing: currentFollowing,
@@ -40,7 +47,7 @@ class SearchController implements IController {
 
   private changeFollow = async (req: Request, res: Response) => {
     const mock = new MockPostService();
-    const currentUser = "billgates";
+    const currentUser = req.user.username;
     const currentFollowing = db.users.filter((user) => user.username === currentUser)[0].following;
     const userId = Number(req.query.userId);
     if (req.body) {
