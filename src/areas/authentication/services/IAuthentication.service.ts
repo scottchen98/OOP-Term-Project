6a663e3822900745ea3d60366a7c51a78d149ded
Scott Chen <scott_chen@Scotts-MacBook-Pro.local>
2023-03-22 14:37:00 -0700

    create SignUp interface for createUser

--- src/areas/authentication/services/IAuthentication.service.ts
+++ src/areas/authentication/services/IAuthentication.service.ts
@@ -1,12 +1,20 @@
 // import IPost from "./post.interface";
 import IUser from "../../../interfaces/user.interface";
 
+interface SignUp {
+  firstName: string;
+  lastName: string;
+  username: string;
+  email: string;
+  password: string;
+}
+
 // ⭐️ Feel free to change this interface in any way you like. It is simply an example...
 export interface IAuthenticationService {
   _db: any;
   findUserByEmail(email: String): Promise<IUser | null>;
 
-  createUser(user: IUser): Promise<IUser | undefined>;
+  createUser(user: SignUp): Promise<IUser | undefined>;
 
   getUserByEmailAndPassword(email: string, password: string): Promise<IUser | undefined>;
 }
