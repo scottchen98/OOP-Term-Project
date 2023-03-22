ca97dd132955f355b3dbeefbb13656cc7c3417e2
Scott Chen <scott_chen@Scotts-MacBook-Pro.local>
2023-03-22 13:56:29 -0700

    fix type checking

--- src/areas/search/controllers/search.controller.ts
+++ src/areas/search/controllers/search.controller.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import { Request, Response, NextFunction, Router } from "express";
 import IController from "../../../interfaces/controller.interface";
-import IPostService from "../../post/services/IPostService";
 import { database as db } from "../../../model/fakeDB";
 import { MockPostService } from "../../post/services/Post.service.mock";
 import { ensureAuthenticated } from "../../../middleware/authentication.middleware";
@@ -24,36 +23,40 @@ class SearchController implements IController {
   }
 
   private search = async (req: Request, res: Response) => {
-    const currentUser = req.user.username;
-    const mock = new MockPostService();
-    const searchFor = String(req.query.query);
-    const resultUsers = mock.searchUser(searchFor);
-    const resultPosts = mock.searchPost(searchFor);
-    const currentFollowing = db.users.filter((user) => user.username === currentUser)[0].following;
+    if (req.user) {
+      const currentUser = req.user.username;
+      const mock = new MockPostService();
+      const searchFor = String(req.query.query);
+      const resultUsers = mock.searchUser(searchFor);
+      const resultPosts = mock.searchPost(searchFor);
+      const currentFollowing = db.users.filter((user) => user.username === currentUser)[0].following;
 
-    // format resultUsers and resultPosts
-    const formattedUsers = resultUsers.map((user: IUser) => new SearchUserViewModel(user).resultUser);
-    const formattedPosts = resultPosts.map((post: IPost) => new SearchPostViewModel(post).resultPost);
+      // format resultUsers and resultPosts
+      const formattedUsers = resultUsers.map((user: IUser) => new SearchUserViewModel(user).resultUser);
+      const formattedPosts = resultPosts.map((post: IPost) => new SearchPostViewModel(post).resultPost);
 
-    res.render("search/views/search", {
-      resultUsers: formattedUsers,
-      resultPosts: formattedPosts,
-      getUsernameById: mock.getUsernameById,
-      checkFollowing: mock.checkFollowing,
-      currentFollowing: currentFollowing,
-      currentUser: currentUser,
-    });
+      res.render("search/views/search", {
+        resultUsers: formattedUsers,
+        resultPosts: formattedPosts,
+        getUsernameById: mock.getUsernameById,
+        checkFollowing: mock.checkFollowing,
+        currentFollowing: currentFollowing,
+        currentUser: currentUser,
+      });
+    }
   };
 
   private changeFollow = async (req: Request, res: Response) => {
-    const mock = new MockPostService();
-    const currentUser = req.user.username;
-    const currentFollowing = db.users.filter((user) => user.username === currentUser)[0].following;
-    const userId = Number(req.query.userId);
-    if (req.body) {
-      mock.changeFollow(userId, currentUser);
+    if (req.user) {
+      const mock = new MockPostService();
+      const currentUser = req.user.username;
+      const currentFollowing = db.users.filter((user) => user.username === currentUser)[0].following;
+      const userId = Number(req.query.userId);
+      if (req.body) {
+        mock.changeFollow(userId, currentUser);
+      }
+      res.redirect("back");
     }
-    res.redirect("back");
   };
 }
 export default SearchController;
