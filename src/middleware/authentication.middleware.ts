bd5184fb1491e00d6019427dad1637e72d9017ff
Scott Chen <scott_chen@Scotts-MacBook-Pro.local>
2023-03-23 22:24:51 -0700

    add type checking

--- src/middleware/authentication.middleware.ts
+++ src/middleware/authentication.middleware.ts
@@ -19,10 +19,12 @@ export const checkPostPrivilege = (req: Request, res: Response, next: NextFuncti
   const mock = new MockPostService();
   const post = mock.findById(Number(req.params.id));
 
-  if (req.user.id === post.userId) {
-    return next();
-  } else if (req.user.following.includes(post.userId)) {
-    return next();
+  if (req.user) {
+    if (req.user.id === post.userId) {
+      return next();
+    } else if (req.user.following.includes(post.userId)) {
+      return next();
+    }
   }
   res.redirect("/posts");
 };
